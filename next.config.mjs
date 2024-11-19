8d9a0a5dac7f02028cd4c55adfb3cc70e3443205
Naveen <naveenanand173@gmail.com>
2024-11-19 23:05:26 +0530

    vercel file is modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+    appDir: true, // Ensures the app folder is properly handled
+  },
+};
 
 export default nextConfig;
